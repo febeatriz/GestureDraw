a2b6a47043b421f36e62c7d786e1202dff7c2f57
febeatriz <febiatx@gmail.com>
2026-06-15 21:45:43 -0300

    changing color and delete empty files

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -4,7 +4,7 @@ import cv2
 WIDTH, HEIGHT = 1280, 720
 
 # Cores (BGR)
-COLOR_BRUSH = (255, 0, 0)   # Azul
+COLOR_BRUSH = (247, 5, 157)   # Azul
 COLOR_ERASER = (0, 0, 0)    # Preto (para o canvas)
 COLOR_CURSOR = (0, 255, 0)  # Verde
 
